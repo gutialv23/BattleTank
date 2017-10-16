aa6807070da148d406676813f9754f619d739ffc
gutialv23 <alvaro_guti_777@hotmail.com>
2017-10-16 19:23:22 +0200

    BT12 Converting our TankPlayerController and Tank blueprints into cpp classes and starting to build a new Player Controller

---
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Tank.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+	ATank *GetControlledTank() const;
+	
+};
